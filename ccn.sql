2a745e283b571172044b8487206d1a468e1afed7
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-02 23:20:36 +0545

    Refactor SQL script for improved readability and structure in UserDetails and UserLocationDetails table creation

--- ccn.sql
+++ ccn.sql
@@ -1,19 +1,20 @@
 USE master;
+
 DROP DATABASE CCN;
-GO
-CREATE DATABASE CCN;
-GO
-USE CCN;
-CREATE TABLE UserDetails
-(
-    Id INT IDENTITY(106,100) PRIMARY KEY NOT NULL ,
-    UserName VARCHAR
-    (100) NOT NULL,
-    Address VARCHAR 
-    (100)
-);
-INSERT INTO UserDetails
-    (UserName, Address)
+
+GO CREATE DATABASE CCN;
+
+GO USE CCN;
+
+CREATE TABLE
+    UserDetails (
+        Id INT IDENTITY (106, 100) PRIMARY KEY NOT NULL,
+        UserName VARCHAR(100) NOT NULL,
+        Address VARCHAR(100)
+    );
+
+INSERT INTO
+    UserDetails (UserName, Address)
 VALUES
     ('cat', 'nepal'),
     ('rat', 'nepal'),
@@ -21,52 +22,53 @@ VALUES
     ('fat', 'nepal'),
     ('sat', 'nepal');
 
-SELECT *
-FROM UserDetails
-PRINT '---------------------------------------------'
-
-SELECT Id
-FROM UserDetails
-WHERE Id<306
-
-PRINT '---------------------------------------------'
-
-INSERT INTO UserDetails
-    (UserName,Address)
+SELECT
+    *
+FROM
+    UserDetails PRINT '---------------------------------------------'
+SELECT
+    Id
+FROM
+    UserDetails
+WHERE
+    Id < 306 PRINT '---------------------------------------------'
+INSERT INTO
+    UserDetails (UserName, Address)
 VALUES
-    ( 'wee', 'weeee');
+    ('wee', 'weeee');
 
-SELECT *
-FROM UserDetails
+SELECT
+    *
+FROM
+    UserDetails PRINT '---------------------------------------------'
+    -- UPDATE UserDetails SET Address='weeeeeeeeeeeeee' -- sab ly select gorxa 
+UPDATE UserDetails
+SET
+    Address = 'weeeeeeeeeeeeee'
+WHERE
+    Id > 506
+SELECT
+    *
+FROM
+    UserDetails PRINT '---------------------------------------------'
+    -- DELETE * FROM UserDetails --well it sems 
+DELETE FROM UserDetails
+WHere
+    Id = 106;
 
-PRINT '---------------------------------------------'
+SELECT
+    *
+FROM
+    UserDetails PRINT '---------------------------------------------'
+CREATE TABLE
+    UserLocationDetails (
+        L_Id INT IDENTITY (100, 1) PRIMARY KEY NOT NULL,
+        L_Name VARCHAR(70),
+        L_Description VARCHAR(MAX)
+    );
 
--- UPDATE UserDetails SET Address='weeeeeeeeeeeeee' -- sab ly select gorxa 
-UPDATE UserDetails SET Address='weeeeeeeeeeeeee' WHERE Id>506
-
-SELECT *
-FROM UserDetails
-
-PRINT '---------------------------------------------'
--- DELETE * FROM UserDetails --well it sems 
-
-DELETE FROM UserDetails WHere Id=106;
-
-SELECT *
-FROM UserDetails
-
-
-PRINT '---------------------------------------------'
-
-
-CREATE TABLE UserLocationDetails
-(
-    L_Id INT IDENTITY(100,1) PRIMARY KEY NOT NULL,
-    L_Name VARCHAR(70),
-    L_Description VARCHAR(MAX)
-);
-INSERT INTO UserLocationDetails
-    (L_Name,L_Description)
+INSERT INTO
+    UserLocationDetails (L_Name, L_Description)
 VALUES
     ('nepal', 'wow verry buutifull'),
     ('nepal', 'wow verry buutifull'),
@@ -74,11 +76,13 @@ VALUES
     ('nepal', 'wow verry buutifull'),
     ('nepal', 'wow verry buutifull');
 
-SELECT *
-FROM UserLocationDetails;
+SELECT
+    *
+FROM
+    UserLocationDetails;
 
-SELECT Address
-FROM UserDetails
-    RIGHT JOIN UserLocationDetails
-    ON UserDetails.Address=UserLocationDetails.L_Name;
-
+SELECT
+    Address
+FROM
+    UserDetails
+    RIGHT JOIN UserLocationDetails ON UserDetails.Address = UserLocationDetails.L_Name;
