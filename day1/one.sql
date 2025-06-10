daf678430bcb34e6c91d0e633f2f8b55dbd8fe48
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-10 10:21:45 +0545

    Add initial SQL script for inserting data into tbl_std and managing records

---
+++ day1/one.sql
@@ -0,0 +1,46 @@
+-- CREATE DATABASE NepathyaDB
+use NepathyaDB
+-- CREATE TABLE
+--     tbl_std (
+--         Id INT IDENTITY (100, 1) PRIMARY KEY,
+--         StdName VARCHAR(100) not NULL,
+--         address text,
+--         phoneNUm VARCHAR(15),
+--         gender VARCHAR(8) not NULL,
+--         email VARCHAR(225)
+--     )
+INSERT INTO
+    tbl_std (StdName, address, phoneNUm, gender, email)
+VALUES
+    (
+        'ccn',
+        'skn-12',
+        + 999991920,
+        'mail',
+        'aww@wee.wee'
+    ),
+    (
+        'cat',
+        'snn-12',
+        + 974567367,
+        'maIil',
+        'wee@wee.wee'
+    )
+SELECT
+    *
+FROM
+    tbl_std
+    --------------------------------------------
+DELETE FROM tbl_std
+WHERE
+    Id > 100
+    ----------------------------------------------
+SELECT
+    *
+FROM
+    tbl_std
+
+
+    -- DROP TABLE std_tbl
+
+    
