1bd8f237d87fcf1f4c13fdcc6b87b51c9eb40549
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-16 10:12:53 +0545

    Add SQL scripts for selecting distinct student names and top 3 records from Std_det table

---
+++ day3/distinct.sql
@@ -0,0 +1,8 @@
+use NepathyaDB
+SELECT DISTINCT
+    stdName
+
+FROM
+    Std_det
+ORDER BY stdName
+    -- col name change garrera dehako nothin special
---
+++ day3/top3.sql
@@ -0,0 +1,7 @@
+use NepathyaDB
+SELECT TOP 3
+    *
+FROM
+    Std_det
+WHERE CONVERT(varchar,stdAddress)='khausali'
+    -- col name change garrera dehako nothin special
