74cda94491e51fe225f406add9890234a98ac2a5
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-26 10:34:51 +0545

    Add SQL trigger documentation outlining timing options and events

---
+++ day8--Triggers/triggers.sql
@@ -0,0 +1,12 @@
+-- ## Triggers
+-- A trigger in SQL is a special kind of stored procedure that automatically executes when a specified event occurs in the DB
+
+-- ### trigger timing options
+-- - Before
+-- - after
+-- - instead of
+
+-- ### Trigger Events
+-- - Insert
+-- - Update
+-- - Delete
